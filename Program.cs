dd04597a47920eba14a8cb0ce2ec2b148c5dc9f6
Kenneth Thompson <ken.thompson23@gmail.com>
2012-04-04 22:05:27 -0400

    - initial commit to parse and build a table consisting of the 8 required tables.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQL_Server_GTFS_builder_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
